010031b6c66159513cf838b45eecfd4d9d4feb11
shen <1571817467@qq.com>
2023-11-25 13:53:24 +0800

    TextView

---
+++ app/src/main/java/com/example/androidstudiostudy/TextActivity.java
@@ -0,0 +1,14 @@
+package com.example.androidstudiostudy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TextActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_text2);
+    }
+}
