931faf2d4e6e2df2d2c8dae235f43882970dd9e2
shen <1571817467@qq.com>
2023-11-27 10:59:16 +0800

    SeekBar

--- app/src/main/java/com/example/androidstudiostudy/RadioButtonActivity.java
+++ app/src/main/java/com/example/androidstudiostudy/RadioButtonActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.CompoundButton;
+import android.widget.SeekBar;
 import android.widget.ToggleButton;
 
 public class RadioButtonActivity extends AppCompatActivity {
@@ -19,6 +20,33 @@ public class RadioButtonActivity extends AppCompatActivity {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 Log.e("isChecked","当前开关触发器的状态："+isChecked);
 
+            }
+        });
+
+        SeekBar seekBar = findViewById(R.id.seekBar);
+        // 设置最大值
+        seekBar.setMax(50);
+        // 设置当前进度
+        seekBar.setProgress(40);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            // 进度改变时（过程中）的回调方法
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                Log.e("seekBar change","当前seekBar的进度："+progress);
+
+            }
+
+            // 开始时回调的方法
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                Log.e("seekBar开始了","当前seekBar的进度："+seekBar.getProgress());
+
+            }
+
+            // 结束时回调的方法
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                Log.e("seekBar结束了","当前seekBar的进度："+seekBar.getProgress());
             }
         });
     }
