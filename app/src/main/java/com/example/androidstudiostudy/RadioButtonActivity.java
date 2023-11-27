6505cea08a0f5ce856583e4761bbed30ed48d17d
shen <1571817467@qq.com>
2023-11-27 10:46:54 +0800

    ToggleButton 开关触发器

--- app/src/main/java/com/example/androidstudiostudy/RadioButtonActivity.java
+++ app/src/main/java/com/example/androidstudiostudy/RadioButtonActivity.java
@@ -3,6 +3,9 @@ package com.example.androidstudiostudy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.CompoundButton;
+import android.widget.ToggleButton;
 
 public class RadioButtonActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class RadioButtonActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_radio_button);
+        ToggleButton toggleButton = findViewById(R.id.toggleButton1);
+        toggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                Log.e("isChecked","当前开关触发器的状态："+isChecked);
+
+            }
+        });
     }
 }
