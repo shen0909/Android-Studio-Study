9f07705961906a08354f5c6a46f18834c3e61683
shen <1571817467@qq.com>
2023-11-28 20:23:43 +0800

    PopupWindow 弹窗
    1.准备弹窗视图,实例化 PopupWindow 对象
    2.设置弹窗，点击事件、背景等
    3.显示
    
    试试将封装弹窗类，将点击事件写在另一个类中

--- app/src/main/java/com/example/androidstudiostudy/ButtonActivity.java
+++ app/src/main/java/com/example/androidstudiostudy/ButtonActivity.java
@@ -3,16 +3,19 @@ package com.example.androidstudiostudy;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.PopupMenu;
-
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.ActionMode;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.PopupWindow;
 import android.widget.Toast;
 
 public class ButtonActivity extends AppCompatActivity implements View.OnClickListener {
@@ -144,6 +147,7 @@ public class ButtonActivity extends AppCompatActivity implements View.OnClickLis
     //点击弹出对话框方法
     public void popAlert(View view) {
         int id = view.getId();
+        // 弹出普通对话框
         if (id == R.id.alt_btn1) {
             // 方法一：用构建器创建对话框：先创建对话框再显示
             AlertDialog.Builder builder = new AlertDialog.Builder(this);
@@ -161,6 +165,70 @@ public class ButtonActivity extends AppCompatActivity implements View.OnClickLis
             MyDialog myDialog = new MyDialog(this,R.style.Mydialog);
             myDialog.show();
         }
+        // 点击弹出弹窗
+        else if (id == R.id.popw_btn) {
+            showPopupWindow(view);
+        }
+    }
+
+    //设置PopupWindow
+    public void showPopupWindow(View anchorView){
+
+        // 1.实例化 PopupWindow 对象
+        /* 参数一 用在弹窗中的View(注意不是布局资源id,所以要根据布局资源id转换成view)
+         * 参数二/三 长宽
+         * 参数四 是否获取焦点*/
+        // 1.1 准备弹窗需要的视图
+        /* 根据 id = R.layout.popuop_layout 布局文件创建一个视图View,在 环境上下文 中使用*/
+        /* 布局文件xml和视图之间的关系
+         * 布局文件(.xml文件)定义了UI元素的结构和外观，而View对象则代表了布局中的一个具体的UI元素。
+         * 通过使用LayoutInflater类，我们可以将布局文件解析成一个View对象，然后将布局文件转换为实际可见的 UI 元素
+         * 通过指定上下文(this)来获取当前上下文环境的LayoutInflater实例
+         * nflate(R.layout.popuop_layout, null)是LayoutInflater类的方法，它用于将布局文件R.layout.popuop_layout转换为一个View对象。*/
+        View v = LayoutInflater.from(this).inflate(R.layout.popuop_layout,null);
+        PopupWindow popupWindow = new PopupWindow(v,400,70,false);
+
+        // 2.设置
+        // 2.1 设置背景
+        popupWindow.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));// 透明色
+        // 2.2 设置能响应外部的点击事件
+        popupWindow.setOutsideTouchable(true);
+        // 2.3 设置弹窗能响应点击事件
+        popupWindow.setTouchable(true);
+        // 2.4 为弹窗中的文本设计点击事件
+        /* 一般设计点击事件需要用 findViewById() 去找到控件的id,从而找到控件，再为控件绑定点击事件
+         * 但此时我们想要找的控件不在当前视图中,它在 R.layout.popuop_layout 布局中，所以我们要在由它创建的视图中找控件
+         * 当前activity绑定的布局是 R.layout.activity_button */
+        v.findViewById(R.id.sele).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(ButtonActivity.this,"点击了选择",Toast.LENGTH_SHORT).show();
+                popupWindow.dismiss();
+            }
+        });
+
+        v.findViewById(R.id.all_sele).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(ButtonActivity.this,"点击了全选",Toast.LENGTH_SHORT).show();
+                popupWindow.dismiss();
+            }
+        });
+
+        v.findViewById(R.id.cope).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(ButtonActivity.this,"点击了复制",Toast.LENGTH_SHORT).show();
+                popupWindow.dismiss();
+            }
+        });
+
+        // 3.显示
+        /* View anchor, int xoff, int yoff
+         * 参数一 在哪里显示
+         * x偏移
+         * y偏移*/
+        popupWindow.showAsDropDown(anchorView);
     }
 
     // 自定义一个 继承 View.OnClickListener 接口的类，实现里面的方法
