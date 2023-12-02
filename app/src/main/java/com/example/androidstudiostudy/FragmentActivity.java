f3f358fdcedd639a5bae2a83f49998046d7a6dbe
shen <1571817467@qq.com>
2023-12-02 13:49:41 +0800

    fragment 动态加载 - 用Java代码添加 --替换fragment FragmentTransaction.replace() 方法替换 Fragment 参数1：containerViewId 应放置片段的位置 参数2：要替换的片段

--- app/src/main/java/com/example/androidstudiostudy/FragmentActivity.java
+++ app/src/main/java/com/example/androidstudiostudy/FragmentActivity.java
@@ -3,8 +3,8 @@ package com.example.androidstudiostudy;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
-
 import android.os.Bundle;
+import android.view.View;
 
 // 创建一个新的activity 绑定布局 R.layout.activity_fragment ,在该布局里添加 fragment 控件，进行展示
 public class FragmentActivity extends AppCompatActivity {
@@ -20,6 +20,17 @@ public class FragmentActivity extends AppCompatActivity {
         // 使用add()方法添加一个fragment片段
         Fragment2 fragment2 = Fragment2.newInstance(null,null);
         fragmentTransaction.add(R.id.fragment2,fragment2);
+        fragmentTransaction.commit();
+    }
+
+    // FragmentTransaction.replace() 方法替换 Fragment
+    // 参数1：containerViewId 应放置片段的位置
+    // 参数2：要替换的片段
+    public void replaceFragment(View view) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        Fragment1 newFragment = new Fragment1();
+        fragmentTransaction.replace(R.id.fragment2,newFragment);
         fragmentTransaction.commit();
     }
 }
