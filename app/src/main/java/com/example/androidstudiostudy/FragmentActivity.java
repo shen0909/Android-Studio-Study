8a57c814931b56aee438a6565038699997f72bc9
shen <1571817467@qq.com>
2023-12-02 11:01:23 +0800

    fragment 动态加载 - 用Java代码添加
    1.在布局文件中添加存放fragment的container，有一个专门的container：androidx.fragment.app.FragmentContainerView
    2.在Java文件中添加以下代码
    FragmentManager fragmentManager = getSupportFragmentManager();
    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
    fragmentTransaction.add(参数1,参数2);
    参数1：ViewGroup，即应放置片段的位置，由资源 ID 指定
    参数2：要添加的片段
    3.一旦通过 FragmentTransaction 做出了更改，就必须调用 commit() 以使更改生效

--- app/src/main/java/com/example/androidstudiostudy/FragmentActivity.java
+++ app/src/main/java/com/example/androidstudiostudy/FragmentActivity.java
@@ -1,6 +1,9 @@
 package com.example.androidstudiostudy;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
 import android.os.Bundle;
 
 // 创建一个新的activity 绑定布局 R.layout.activity_fragment ,在该布局里添加 fragment 控件，进行展示
@@ -10,5 +13,13 @@ public class FragmentActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fragment);
+
+        // 用Java代码添加fragment
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        // 使用add()方法添加一个fragment片段
+        Fragment2 fragment2 = Fragment2.newInstance(null,null);
+        fragmentTransaction.add(R.id.fragment2,fragment2);
+        fragmentTransaction.commit();
     }
 }
