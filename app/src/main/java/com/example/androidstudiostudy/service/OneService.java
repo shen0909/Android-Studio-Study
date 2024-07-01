c1afa16e9cf573b92955de8a30e103d8f38a2e5a
shen <1571817467@qq.com>
2024-07-01 16:36:22 +0800

    绑定服务 - 扩展Binder类

--- app/src/main/java/com/example/androidstudiostudy/service/OneService.java
+++ app/src/main/java/com/example/androidstudiostudy/service/OneService.java
@@ -2,29 +2,57 @@ package com.example.androidstudiostudy.service;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Binder;
 import android.os.IBinder;
 import android.util.Log;
 
 //创建 Service 的子类（或使用它的一个现有子类如IntentService）,重写一些回调方法
 public class OneService extends Service {
-    public OneService() {
-    }
+    private OneServiceBinder oneServiceBinder = new OneServiceBinder();
+    private Thread thread;
 
     // 当另一个组件想通过调用 bindService() 与服务绑定（例如执行 RPC）时，系统将调用此方法。
     // 在此方法的实现中，必须返回 一个IBinder 接口的实现类，供客户端用来与服务进行通信。
     // 无论是启动状态还是绑定状态，此方法必须重写，但在启动状态的情况下直接返回 null。
     @Override
     public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+        return oneServiceBinder;
+    }
+
+    /**
+     * 创建Binder对象，返回给客户端即Activity使用，提供数据交换的接口
+     */
+    public class OneServiceBinder extends Binder {
+        // 声明一个方法，getService。（提供给客户端调用）
+        OneService getService() {
+            // 返回当前对象LocalService,这样我们就可在客户端端调用Service的公共方法了
+            return OneService.this;
+        }
     }
 
     // 首次创建服务时，系统将调用此方法来执行一次性设置程序（在调用 onStartCommand() 或onBind() 之前）。
     // 如果服务已在运行，则不会调用此方法，该方法只调用一次
+    private int count = 0;
+    private boolean quit = false;
+
     @Override
     public void onCreate() {
         super.onCreate();
-        Log.d("服务","首次创建服务调用此方法来执行一次性设置程序,该方法只调用一次");
+        Log.d("服务", "首次创建服务调用此方法来执行一次性设置程序,该方法只调用一次");
+        thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (!quit) {
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    count++;
+                }
+            }
+        });
+        thread.start();
     }
 
     // 当另一个组件（如 Activity）通过调用 startService() 请求启动服务时，系统将调用此方法。
@@ -32,7 +60,7 @@ public class OneService extends Service {
     // （在绑定状态下，无需实现此方法。）
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        Log.d("服务","onStartCommand");
+        Log.d("服务", "onStartCommand");
         return super.onStartCommand(intent, flags, startId);
     }
 
@@ -40,6 +68,19 @@ public class OneService extends Service {
     @Override
     public void onDestroy() {
         super.onDestroy();
-        Log.d("服务","销毁服务");
+        this.quit = true;
+        Log.d("服务", "销毁服务");
+    }
+
+    //--------------------公共方法------------------
+    public int getCount() {
+        return count;
+    }
+    //--------------------解除绑定时调用------------------
+
+    @Override
+    public boolean onUnbind(Intent intent) {
+        Log.d("服务", "解除绑定");
+        return super.onUnbind(intent);
     }
 }
--- app/src/main/java/com/example/androidstudiostudy/service/StudyService.java
+++ app/src/main/java/com/example/androidstudiostudy/service/StudyService.java
@@ -1,17 +1,40 @@
 package com.example.androidstudiostudy.service;
 
+import android.app.Service;
+import android.content.ComponentName;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.util.Log;
 import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.androidstudiostudy.R;
 
 public class StudyService extends AppCompatActivity {
 
+    private ServiceConnection serviceConnection;
+    private OneService myService;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_study_service);
+
+        serviceConnection = new ServiceConnection() {
+            // 绑定成功时调用
+            @Override
+            public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+                Log.d("绑定服务","成功绑定服务");
+                OneService.OneServiceBinder oneServiceBinder = (OneService.OneServiceBinder) iBinder;
+                myService = oneServiceBinder.getService();
+            }
+            // Android 系统会在与服务的连接意外中断时（例如当服务崩溃或被终止时）调用该方法
+            @Override
+            public void onServiceDisconnected(ComponentName componentName) {
+                Log.d("绑定服务","与服务的连接意外中断");
+                myService = null;
+            }
+        };
 
     }
 
@@ -20,6 +43,15 @@ public class StudyService extends AppCompatActivity {
         Intent intent = new Intent(this,OneService.class);
         if(id == R.id.bindService){
             // 绑定service
+            bindService(intent,serviceConnection,Service.BIND_AUTO_CREATE);
+            if (myService != null) {
+                // 通过绑定服务传递的Binder对象，获取Service暴露出来的数据
+
+                Log.d("获取绑定数据", "从服务端获取数据：" + myService.getCount());
+            } else {
+
+                Log.d("获取绑定数据", "还没绑定呢，先绑定,无法从服务端获取数据");
+            }
         } else if (id == R.id.stopService) {
             stopService(intent);
         } else {
