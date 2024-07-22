41138dc4510f96dca6645f5dec2d2c7e55a1c52b
shen <1571817467@qq.com>
2024-07-22 16:30:56 +0800

    Android - 广播 （有序、无序）

--- app/src/main/java/com/example/androidstudiostudy/broadcastreceiver/BroadcastStudy.java
+++ app/src/main/java/com/example/androidstudiostudy/broadcastreceiver/BroadcastStudy.java
@@ -7,13 +7,16 @@ import android.content.IntentFilter;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
+
 import com.example.androidstudiostudy.R;
 
 public class BroadcastStudy extends AppCompatActivity {
 
     public String SEND_STATIC_ACTION = "send_static_action";
     public String SEND_DYNAMIC_ACTION = "send_dynamic_action";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,37 +25,69 @@ public class BroadcastStudy extends AppCompatActivity {
         // 动态注册广播接收器
         IntentFilter intentFilter = new IntentFilter();
         intentFilter.addAction(SEND_DYNAMIC_ACTION);
-        registerReceiver(dynamicBroadcastReceiver,intentFilter);
-    }
+        intentFilter.setPriority(100);
+        registerReceiver(dynamicBroadcastReceiver, intentFilter);
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        unregisterReceiver(dynamicBroadcastReceiver);
+        // 注册有序广播
+        // 有序广播设置优先级，优先级越大越先收到
+        IntentFilter intentFilter1 = new IntentFilter();
+        intentFilter1.addAction(SEND_DYNAMIC_ACTION);
+        intentFilter1.setPriority(101);
+        registerReceiver(broadcastReceiver2, intentFilter1);
+
     }
 
     // 创建广播接收器
     private final BroadcastReceiver dynamicBroadcastReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
-            Bundle bundle = intent.getExtras();
-            Log.d("BroadcastReceiver","动态广播接收器接受了一个 - "+bundle.getString("data"));
+            // Bundle bundle = intent.getExtras();
 
+            // 下游使用 getResultExtras  接收 Bundle，使用 getResultData 接收字符串
+            Bundle bundle = getResultExtras(true);
+            String newdata = getResultData();
+            Log.d("BroadcastReceiver", "动态广播接收器1接受了一个 - " + bundle.getString("new_data")+"\t"+newdata);
+        }
+    };
+
+    private final BroadcastReceiver broadcastReceiver2 = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Bundle bundle = intent.getExtras();
+            Log.d("BroadcastReceiver", "动态广播接收器2接受了一个 - " + bundle.getString("data"));
+            // 截断广播
+            // abortBroadcast();
+
+            // 修改广播
+            // broadcastReceiver2 优先级高，所以是上游广播接收器，可以使用 setResultExtras 传递一个新的 Bundle 或者使用 setResultData 传递一个字符串
+            bundle.putString("new_data","新信息");
+            setResultExtras(bundle);
+            setResultData("上游传递了一个字符串");
         }
     };
 
     public void sendBroadcast(View view) {
-        if(view.getId() == R.id.send_static){
+        if (view.getId() == R.id.send_static) {
             Intent intent = new Intent();
             intent.setAction(SEND_STATIC_ACTION);
-            intent.putExtra("data","静态广播");
+            intent.putExtra("data", "静态广播");
             sendBroadcast(intent);
         }
-        if(view.getId() == R.id.send_dynamic){
+        if (view.getId() == R.id.send_dynamic) {
             Intent intent = new Intent();
             intent.setAction(SEND_DYNAMIC_ACTION);
-            intent.putExtra("data","动态广播");
-            sendBroadcast(intent);
+            intent.putExtra("data", "广播-11111111");
+            // sendBroadcast(intent);
+
+            // 发送有序广播
+            sendOrderedBroadcast(intent, null);
         }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(dynamicBroadcastReceiver);
+        unregisterReceiver(broadcastReceiver2);
     }
 }
