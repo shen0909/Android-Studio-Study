59d87dcf5590354a3474b68549d4005b79184d42
shen <1571817467@qq.com>
2024-07-24 16:08:38 +0800

    Android -- 内容提供者

--- app/src/main/java/com/example/androidstudiostudy/contentprovider/MyContentProvider.java
+++ app/src/main/java/com/example/androidstudiostudy/contentprovider/MyContentProvider.java
@@ -4,51 +4,84 @@ import android.content.ContentProvider;
 import android.content.ContentValues;
 import android.content.UriMatcher;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 
 import com.example.androidstudiostudy.database.DataBaseHelper;
 
 public class MyContentProvider extends ContentProvider {
-    private UriMatcher uriMatcher;
+    private UriMatcher uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+    private String AUTHORITIES = "com.example.androidstudiostydy.provider";
+    private DataBaseHelper dataBaseHelper;
+    private SQLiteDatabase sqLiteDatabase;
+
     public MyContentProvider() {
     }
 
     @Override
-    public int delete(Uri uri, String selection, String[] selectionArgs) {
-        // Implement this to handle requests to delete one or more rows.
-        throw new UnsupportedOperationException("Not yet implemented");
+    public boolean onCreate() {
+        dataBaseHelper = new DataBaseHelper(getContext());
+        sqLiteDatabase = dataBaseHelper.getWritableDatabase();
+        uriMatcher.addURI(AUTHORITIES, DataBaseHelper.STUTDY_SQLITE_TABLE, 0);
+        return true;
     }
 
     @Override
     public String getType(Uri uri) {
-        // TODO: Implement this to handle requests for the MIME type of the data
-        // at the given URI.
-        throw new UnsupportedOperationException("Not yet implemented");
-    }
-
-    @Override
-    public Uri insert(Uri uri, ContentValues values) {
-        // TODO: Implement this to handle requests to insert a new row.
-        throw new UnsupportedOperationException("Not yet implemented");
-    }
-
-    @Override
-    public boolean onCreate() {
-        DataBaseHelper dataBaseHelper = new DataBaseHelper(getContext());
-        return false;
+        int code = uriMatcher.match(uri);
+        if (code == 0) {
+            return DataBaseHelper.STUTDY_SQLITE_TABLE;
+        } else {
+            return null;
+        }
     }
 
     @Override
     public Cursor query(Uri uri, String[] projection, String selection,
                         String[] selectionArgs, String sortOrder) {
-        // TODO: Implement this to handle query requests from clients.
-        throw new UnsupportedOperationException("Not yet implemented");
+        String tableName = getType(uri);
+        if (tableName == null) {
+            return null;
+        }
+        return sqLiteDatabase.query(tableName, projection, selection, selectionArgs, null, null, sortOrder);
     }
 
+    /// 删除
+    @Override
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
+        String tableName = getType(uri);
+        // 查询的表不存在
+        if (tableName == null) {
+            return -1;
+        }
+        return sqLiteDatabase.delete(tableName, selection, selectionArgs);
+    }
+
+    /// 更新
     @Override
     public int update(Uri uri, ContentValues values, String selection,
                       String[] selectionArgs) {
-        // TODO: Implement this to handle requests to update one or more rows.
-        throw new UnsupportedOperationException("Not yet implemented");
+        String tableName = getType(uri);
+        // 查询的表不存在
+        if (tableName == null) {
+            return -1;
+        }
+        return sqLiteDatabase.update(tableName, values, selection, selectionArgs);
     }
+
+    @Override
+    public Uri insert(Uri uri, ContentValues values) {
+        String tableName = getType(uri);
+        // 查询的表不存在
+        if (tableName == null) {
+            return null;
+        }
+        long index = sqLiteDatabase.insert(tableName, null, values);
+        if (index > 0) {
+            return uri;
+        } else {
+            return null;
+        }
+    }
+    
 }
