b8625368bbe2e1a9c2af798517032eb704f69b44
shen <1571817467@qq.com>
2024-08-26 15:05:21 +0800

    优化方法

--- app/src/main/java/com/example/androidstudiostudy/customview/OneTextTwoColor.java
+++ app/src/main/java/com/example/androidstudiostudy/customview/OneTextTwoColor.java
@@ -11,7 +11,6 @@ import android.os.Build;
 import android.util.AttributeSet;
 import android.widget.TextView;
 import androidx.annotation.Nullable;
-import androidx.annotation.RequiresApi;
 import com.example.androidstudiostudy.R;
 
 // 一段文字两种颜色，文字变色
@@ -22,6 +21,9 @@ public class OneTextTwoColor extends TextView {
     private int changeColor = Color.BLACK;
     private Paint customPaint, changePaint;
     private double middleNum = 0.5; //给定一个默认的中间值，用来划分
+    private int baseLine; //基线
+    private int text_x; // 原点x坐标
+    private String text;
 
     public OneTextTwoColor(Context context) {
         this(context, null);
@@ -42,6 +44,8 @@ public class OneTextTwoColor extends TextView {
         customColor = array.getColor(R.styleable.OneTextTwoColor_customColor, Color.BLACK);
         array.recycle(); //回收
 
+        text = getText().toString();
+
         customPaint = new Paint();
         customPaint.setColor(customColor);
         customPaint.setAntiAlias(true);
@@ -57,18 +61,35 @@ public class OneTextTwoColor extends TextView {
         super(context, attrs, defStyleAttr, defStyleRes);
     }
 
+    // 绘制文字
+    /* 1.一段文字，两种颜色，给定一个值，从此处开始划分，前后两段用不同的画笔绘制 */
     @Override
     protected void onDraw(Canvas canvas) {
+        calculateBaseLine();
 
-        // 绘制文字
-        /* 一段文字，两种颜色，给定一个值，从此处开始划分，前后两段用不同的画笔绘制*/
-        String text = getText().toString();
+        // 分前后两部分绘制文字
+        // 用 clipOutRect 将画布裁剪成两部分，分别用不同的画笔绘制
+        int middleWidth = (int) (middleNum * getWidth());
+        canvasRect(canvas, 0, middleWidth, customPaint);
+        canvasRect(canvas, middleWidth, getWidth(), changePaint);
+    }
+
+    /// 绘制裁剪区域
+    public void canvasRect(Canvas canvas, int left, int middleWidth, Paint paint) {
+        canvas.save(); // 保存画布
+        canvas.clipRect(left, 0, middleWidth, getHeight());
+        canvas.drawText(text, text_x, baseLine, paint);
+        canvas.restore(); // 释放画布
+    }
+
+    /// 计算基线y坐标 原点x坐标
+    public void calculateBaseLine() {
         Rect bound = new Rect(); // 创建一个 Rect 对象,用于存储文本的边界信息
         changePaint.getTextBounds(text, 0, text.length(), bound); // 用 getTextBounds 方法，计算出指定文本 text 的边界，并将结果存储到 bound 对象中
 
         /* x：文本绘制起始点横坐标 */
         // 宽度的一半 - 文字的一半
-        int x = getWidth() / 2 - bound.width() / 2;
+        text_x = getWidth() / 2 - bound.width() / 2;
 
         /* y: 文本绘制基线纵坐标 */
         // top: 基线到文字顶部的距离
@@ -79,19 +100,6 @@ public class OneTextTwoColor extends TextView {
         Paint.FontMetricsInt fontMetricsInt = changePaint.getFontMetricsInt();
         int middle = (fontMetricsInt.bottom - fontMetricsInt.top) / 2;
         int dz = middle - fontMetricsInt.bottom;
-        int baseLine = middle + dz;
-
-        // 分前后两部分绘制文字
-        // 用 clipOutRect 将画布裁剪成两部分，分别用不同的画笔绘制
-        int middleWidth = (int) (middleNum * getWidth());
-        canvas.save(); // 保存画布
-        canvas.clipRect(0,0,middleWidth,getHeight());
-        canvas.drawText(text, x, baseLine, customPaint);
-        canvas.restore();
-
-        canvas.clipRect(middleWidth,0,getWidth(),getHeight());
-        canvas.drawText(text, x, baseLine, changePaint);
-        canvas.save();
-        canvas.restore(); // 释放画布
+        baseLine = middle + dz;
     }
 }
